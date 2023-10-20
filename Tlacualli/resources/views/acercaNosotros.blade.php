84f61ff89ef78b84f7a22cda58b05c7044b6a0ac
Angelo <crisjmtz@hotmail.com>
2023-10-19 20:26:39 -0600

    Se modifico y añadio el layout de html

---
+++ Tlacualli/resources/views/acercaNosotros.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.mainlayout')
+@section ('content')
+<section id="description">
+    <div>
+        <h1>Somos un equipo enfocado en establecer relaciones de colaboración para sectores de produccion de residuos organicos 
+            y organizaciones de generación de composta
+        </h1>
+        <img src="https://www.unidiversidad.com.ar/cache/rse-1-1728x800c_1000_1100.jpg" alt="Imagen descriptiva" width="900px" height="350px">
+    </div>
+    <br>
+    <div>
+        <h2>Objetivo</h2>
+    </div>
+    <br>
+    <div>
+        <h3>Misión</h3>
+    </div>
+    <br>
+    <div>
+        <h3>Visión</h3>
+    </div>
+    <br>    
+    <div>
+        <h4>Valores</h4>
+    </div>
+</section>
+<aside id="archive">
+    <div>
+        <h3><a href="">Estudios relacionados</a></h3>
+    </div>
+    <div>
+        <h3><a href="">Documentos relevantes</a></h3>
+    </div>
+    <div>
+        <h3><a href="">Nuestras estadisticas</a></h3>
+    </div>
+</aside>
+@stop
---
+++ Tlacualli/resources/views/layouts/mainlayout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de nosotros</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#index">Inicio</a>
+            </li>
+            <li>
+                <a href="#contactUs">Contactanos</a>
+            </li>
+            <li>
+                <a href="#collab">Colaboradores</a>
+            </li>
+            <li>
+                <a href="aboutUs">Acerca de nosotros</a>
+            </li>
+            <li>
+                <a href="join">Unete</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+    <footer>
+        <center>
+            <p>
+            Todos los derechos resevados ®
+            </p>
+            <br>
+            <a href="">Facebook</a>
+            <a href="">Instagram</a>
+            <a href="">Twitter</a>
+        </center>
+    </footer>
+</body>
+</html>
