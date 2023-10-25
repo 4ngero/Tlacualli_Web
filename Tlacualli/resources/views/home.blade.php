408249a5e8bbe9f25fce8078dd7c78fcf2a25cb8
MrHIGLE <121038306@upq.edu.mx>
2023-10-24 19:27:35 -0600

    Se agrego el home

---
+++ Tlacualli/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.Main')
+@section('Content')
+
+
+    <div id="logo">
+        <img src="" alt="">
+    </div>
+
+    <div id="Carusel_1">
+        <img src="https://veracruz.uo.edu.mx/sites/default/files/Desarrollo-Sustentable.jpeg" alt="" width="1500px">
+    </div>
+
+    <div id="descripcion">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, blanditiis mollitia? Officia, laboriosam expedita dolor impedit animi nam id molestiae voluptatem repudiandae minus itaque voluptas aut excepturi exercitationem neque at?</p>
+    </div>
+
+    <div id="Carusel_2">
+        <img src="https://lolmagazine.com.mx/wp-content/uploads/2020/12/Mercado-libre-lanza-categoria-Eco-Friendly.jpeg" alt="">
+    </div>
+
+    @stop
