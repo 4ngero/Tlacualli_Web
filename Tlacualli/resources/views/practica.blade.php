ce8e411a6247d271f978a8d73cfd81b78d2f4482
Angelo <crisjmtz@hotmail.com>
2023-10-31 19:46:58 -0600

    Cambios de practica que se deben borrar o unificar

---
+++ Tlacualli/resources/views/practica.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.mainlayout')
+@section ('content')
+<div >
+    <img id="imagen" src="https://www.fundacionaquae.org/wp-content/uploads/2021/12/fresno-e1639053075597.jpg" alt="">
+</div>
+
+@stop 
